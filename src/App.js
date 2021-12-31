89554a6ddf5d4993efa2d0ee9bf8f67b2c75270e
antonlegkiy <anton.legkiy@gmail.com>
2021-12-31 19:20:05 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const cocktails = [{
+    img: 'https://thewanderlustkitchen.com/wp-content/uploads/2021/10/Espresso-Martini-Recipe-157-Square-720x720.jpg',
+    name: 'Эспрессо Мартини',
+    ingredients: ['1oz Кофейный ликер', '1oz Водка', '1oz Эспрессо', '0.25 Сахарный сироп', 'Вспрыск лимона']
+  }, {
+    img: 'https://autogear.ru/misc/i/gallery/109904/2963385.jpg',
+    name: 'Космополитен',
+    ingredients: ['1.5oz Водка', '1oz Клюквенный сок', '0.5oz Квантро (цитрусовый ликер)', '0.25oz Сок Лайма']
+  }, {
+    img: 'https://cdn.diffords.com/contrib/stock-images/2021/01/6014303495109.jpg',
+    name: 'Доминикана',
+    ingredients: ['1.5oz Кофейный ликер', '1.5oz Выдержанный Ром', 'Сливки']
+  }, {
+    img: 'https://imbibemagazine.com/wp-content/uploads/2017/08/lite-treason-horizontal-crdt-charles-campos.jpg',
+    name: 'Белый Русский',
+    ingredients: ['1oz Водка', '1oz Кофейный ликер', 'Сливки']
+  }];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cocktail-container">
+      {cocktails.map((cocktail) => {
+        return (
+            <div className="cocktail-item">
+              <img src={cocktail.img} alt="cocktail-picture"/>
+              <div className="cocktail-description">
+                <h2>{cocktail.name}</h2>
+                <p>{cocktail.ingredients.join(', ')}</p>
+              </div>
+            </div>
+        );
+      })}
     </div>
   );
 }
